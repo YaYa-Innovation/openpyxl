ecd43db077591e9df1f859c195feb02a7bf9c4bc
YaYa-Innovation <yayainnovation3d@gmail.com>
2023-08-19 20:05:27 +0530

    Git Upload Method

--- create_excel.py
+++ create_excel.py
@@ -1,1 +1,15 @@
 #Create Excel File Through a Python Script
+from openpyxl import Workbook
+import time 
+
+wb=Workbook()
+sheet=wb.active
+
+sheet["A1"]="Tamil"
+sheet["B1"]="English"
+sheet["C1"]="Maths"
+sheet["D1"]="science"
+sheet["E1"]="social science"
+
+
+wb.save("index.xlsx")
