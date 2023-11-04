b9fc903de1aa82b10d6d7b7f77a68730f2f2b74f
YaYa-Innovation <124864562+YaYa-Innovation@users.noreply.github.com>
2023-11-04 13:00:39 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,23 +1,30 @@
-@app.route('/get_filtered_data')
-def get_filtered_data():
-    workbook = load_workbook('static/data.xlsx')
+# app.py
+
+from flask import Flask, render_template, request
+from openpyxl import load_workbook
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/display_rows', methods=['POST'])
+def display_rows():
+    start_row = int(request.form['start_row'])
+    end_row = int(request.form['end_row'])
+
+    workbook = load_workbook(f'{app.config["UPLOAD_FOLDER"]}/sample.xlsx')
     sheet = workbook.active
 
-    start_date_str = request.args.get('startDate')
-    end_date_str = request.args.get('endDate')
+    data = []
+    for i in range(start_row, end_row + 1):
+        if i >= 1 and i <= sheet.max_row:
+            row_data = [sheet.cell(row=i, column=j).value for j in range(1, sheet.max_column + 1)]
+            data.append(row_data)
 
-    start_date = datetime.strptime(start_date_str, '%Y-%m-%d') if start_date_str else None
-    end_date = datetime.strptime(end_date_str, '%Y-%m-%d') if end_date_str else None
+    return render_template('index.html', data=data)
 
-    dates = []
-    values = []
-
-    for row in sheet.iter_rows(values_only=True):
-        date = row[0]
-        date_str = date.strftime('%m/%d/%Y')  # Format changed here
-
-        if (not start_date or date >= start_date) and (not end_date or date <= end_date):
-            dates.append(date_str)
-            values.append(row[1:])
-
-    return jsonify(dates=dates, values=values)
+if __name__ == '__main__':
+    app.run(debug=True)
